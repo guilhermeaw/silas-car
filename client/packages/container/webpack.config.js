cba725a596219a6a3867f66df01d76811cb1a1f0
guilhermeaw <guilherme.w@universo.univates.br>
2022-05-22 20:56:25 -0300

    Adiciona container

---
+++ client/packages/container/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+const packageJson = require('./package.json');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+};
