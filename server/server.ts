2f6ddf7c109598a19abe1c062b740dd2c6fa8149
guimaroes <gabriel.guimaraes@universo.univates.br>
2022-06-18 19:11:55 -0300

    Enabling CORS for all routes

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import router from "./routes.ts";
 
 const PORT = 4000;
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
