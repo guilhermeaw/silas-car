e118542e0f9c6007a590c42802f8f03fd887582e
guilhermeaw <guilherme.w@universo.univates.br>
2022-06-19 18:52:42 -0300

    Ajusta para refazer query de listagem de customizações ao excluir

--- client/src/services/mutations/useDeleteCustomization.ts
+++ client/src/services/mutations/useDeleteCustomization.ts
@@ -1,10 +1,11 @@
 import { useSnackbar } from 'notistack';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
 import api from '../api';
 
 export const useDeleteCustomization = () => {
   const { enqueueSnackbar } = useSnackbar();
+  const queryClient = useQueryClient();
 
   return useMutation(
     (id: string) =>
@@ -19,6 +20,8 @@ export const useDeleteCustomization = () => {
         enqueueSnackbar('Customização excluída com sucesso.', {
           variant: 'success',
         });
+
+        queryClient.refetchQueries(['customizations']);
       },
     },
   );
